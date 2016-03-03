b8dd61649c871f7760d7c73e2b502ec41446f42d
Anton <anton.bil@hz.nl>
2016-03-03 12:34:57 +0100

    cleaned up php

--- EMMVEExtenderExtension.php
+++ EMMVEExtenderExtension.php
@@ -1,25 +1,4 @@
 <?php
-//global $SMWServerURLForSPARQLQuery;
-//$SMWServerURLForSPARQLQuery = 'http://192.168.238.133/index.php/Speciaal:URIResolver';
-/*
-call within a widget with javascript:
-<noinclude>
-Dit is de EM3DNavigator Widget.
-You have to include the script d3.v3.js with the script-tag because it is used in document.ready
-</noinclude>
-<includeonly>
-<script src="../extensions/EM3DNavigator/js/d3.v3.js" charset="utf-8"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-mw.loader.using( ['ext.EM3DNavigator']).done( function () {
-//makeCollapsible gives error. Side-effect: error concerning makeCollapsible disappears
-}
-);//mw.loader
-
-});//document.ready
-</script>
-</includeonly>
-*/
 $wgExtensionCredits['semantic'][] = array(
 
 	'path' => __FILE__,
@@ -29,9 +8,6 @@ $wgExtensionCredits['semantic'][] = array(
 
 	// A description of the extension, which will appear on Special:Version.
 	'description' => 'EMM VE Extension',
-
-	// Alternatively, you can specify a message key for the description.
-	//'descriptionmsg' => 'vueconvertapi-desc',
 
 	// The version of the extension, which will appear on Special:Version.
 	// This can be a number or a string.
