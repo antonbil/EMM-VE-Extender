eb62d0be5cab1626891ddf2f43db590de8066d54
waandershz <waandershz@users.noreply.github.com>
2016-03-11 12:19:49 +0100

    Toegevoegd: verwijderen enkele opties in het menu
    Zie ook JIRA-issue EMT-450

--- js/veExtenderUI.js
+++ js/veExtenderUI.js
@@ -1,3 +1,11 @@
 function loadExtenderUI(){
-  console.log('load extender UI');
+
+	/* remove some menu items */
+	$('.oo-ui-toolbar-tools').find('.oo-ui-tool-name-link').remove(); /* Link */
+	$('.oo-ui-toolbar-tools').find('.ve-test-toolbar-cite').remove(); /* Cite */
+	$('.oo-ui-toolbar-tools').find('.oo-ui-tool-name-meta').remove(); /* Page options > Options */
+	$('.oo-ui-toolbar-tools').find('.oo-ui-tool-name-settings').remove(); /* Page options > Page Settings */
+	$('.oo-ui-toolbar-tools').find('.oo-ui-tool-name-advancedSettings').remove(); /* Page options > Advanced settings */
+	$('.oo-ui-toolbar-tools').find('.oo-ui-tool-name-categories').remove(); /* Page options > Categories */
+	$('.oo-ui-toolbar-tools').find('.oo-ui-tool-name-languages').remove(); /* Page options > Languages */
 }
