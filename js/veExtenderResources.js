752c4815b2a3d9d4ad4f44baa2f2a0ef2afb64c7
Anton <anton.bil@hz.nl>
2016-03-11 15:12:43 +0100

    added dialogue for adding page

--- js/veExtenderResources.js
+++ js/veExtenderResources.js
@@ -83,10 +83,11 @@ function addEMMResources(){
      //console.log(toolbar);
      $( '.ve-test-toolbar-insert' ).after(
          toolbar.$group);
-     createDialog();
+   var queries=veExtenderQueries();
+     createDialog('addresourcedialog',queries.resourcehyperlinks,OO.ui.deferMsg( 'visualeditor-emm-addresourcelabel' )());
 }
 
-function createDialog(){
+function createDialog(dialogName,askQuery, actionName){
 /* Static Properties */
 ve.ui.SearchAndReplaceDialog = function( manager, config ) {
 	// Parent constructor
@@ -97,10 +98,78 @@ ve.ui.SearchAndReplaceDialog = function( manager, config ) {
 
 OO.inheritClass( ve.ui.SearchAndReplaceDialog, ve.ui.FragmentDialog );
 
-ve.ui.SearchAndReplaceDialog.static.name = 'addresourcedialog';
+ve.ui.SearchAndReplaceDialog.static.name = dialogName;
 ve.ui.SearchAndReplaceDialog.static.title = 'Add Resource';
 ve.ui.SearchAndReplaceDialog.static.size = 'medium';
+function editPage(pageName){
+  console.log(pageName);
+         var totaladdress=window.location.href;
+	  var startaddress= totaladdress.substr(0, totaladdress.indexOf('index.php')); 
+	  var win = window.open(startaddress+'index.php?title='+pageName+'&action=formedit​', '_blank');
 
+  /*  var api = new mw.Api();
+
+  api.get( {
+      action: 'ask',
+      parameters:'limit:10000',//check how to increase limit of ask-result; done in LocalSettings.php
+      query: askQuery+'|?Semantic title'//get all pages; include property Semantic title
+  } ).done( function ( data ) {
+  }*/
+}
+var pagenames=[];
+ve.ui.SearchAndReplaceDialog.prototype.getBodyHeight = function () {
+  var dialogthat=this;
+  
+  var api = new mw.Api();
+  // Start a "GET" request
+  console.log('Query:'+askQuery+'|?Semantic title');
+  api.get( {
+      action: 'ask',
+      parameters:'limit:10000',//check how to increase limit of ask-result; done in LocalSettings.php
+      //query was: [[Modification date::+]]|?Modification date|?Heading nl
+      //test-query:[[Category:Context]]|?Modification date|?Heading nl
+      query: askQuery+'|?Semantic title'//get all pages; include property Semantic title
+  } ).done( function ( data ) {
+    console.log(data);
+    //parse data
+    //first get results within data
+      var res=data.query.results;
+      //console.log(res);
+
+      //array to store results
+      var arr=[];
+      //for all objects in result
+      for (prop in res) {
+	  if (!res.hasOwnProperty(prop)) {
+	      //The current property is not a direct property of p
+	      continue;
+	  }
+	  //property defined
+	  //now get pagename and Semantic title (if available)
+	  var pagename=res[prop].fulltext;
+	  var semantictitle=res[prop].printouts['Semantic title'][0];
+	  var title='';
+	  if (semantictitle)
+	    arr.push({ value: semantictitle, data: pagename });
+	  else
+	    arr.push({ value: pagename, data: pagename });
+      }
+
+      pagenames=arr;
+      console.log('search id:'+"#"+dialogName+"id");
+      var complete=$( "#"+dialogName+"id" ).find("input");
+    //store data in inputfields 
+      $(complete).autocomplete({
+	  lookup: pagenames,//pagenames are created at the start of dialog
+	  onSelect: function (suggestion) {
+	    editPage(suggestion.data);
+	    //that.pageid=suggestion.data;
+	  },
+	  appendTo: complete.parentElement
+	});
+  });
+  return 400;
+}
 ve.ui.SearchAndReplaceDialog.prototype.initialize = function () {
 	ve.ui.SearchAndReplaceDialog.super.prototype.initialize.call( this );
 	this.panel = new OO.ui.PanelLayout( { '$': this.$, 'scrollable': true, 'padded': true } );
@@ -112,80 +181,23 @@ ve.ui.SearchAndReplaceDialog.prototype.initialize = function () {
 		{ '$': this.$, 'multiline': false, 'placeholder': 'resource' }
 	);
 	//add DOM-id to parent of input-field
-	this.subject.$element.attr("id","oo-ui-tool-resourceId");
+	this.subject.$element.attr("id",dialogName+"id");
 	this.subjectField = new OO.ui.FieldLayout( this.subject, {
 		'$': this.$,
 		'label': 'Subject'
 	} );
-	this.toInput = new OO.ui.TextInputWidget(
-		{ '$': this.$, 'multiline': false, 'placeholder': 'tekst placeholder 2' }
-	);
-	this.toField = new OO.ui.FieldLayout( this.toInput, {
-		'$': this.$,
-		'label': 'to label 1'
-	} );
-
-     var radioSelectInput = new OO.ui.RadioSelectInputWidget( {
-         options: [
-             { data: 'a', label: 'First' },
-             { data: 'b', label: 'Second'},
-             { data: 'c', label: 'Third' }
-         ]
-     } );
-     var layout = new OO.ui.HorizontalLayout( {
-       items: [
-         new OO.ui.LabelWidget( { label: 'Label' } ),
-         new OO.ui.TextInputWidget( { value: 'Text' } )
-       ]
-     } );
-//$( 'body' ).append( fieldset.$element );
-	this.matchCaseCheckbox = new OO.ui.CheckboxInputWidget( {
-		'$': this.$
-	} );
-	var matchCaseField = new OO.ui.FieldLayout( this.matchCaseCheckbox, {
-		'$': this.$,
-		'align': 'inline',
-		'label': 'match label 1'
-	} );
 	ve.ui.SearchAndReplaceDialog.static.actions = [
 
-	{ action: 'save', label: 'Insert', flags: [ 'primary', 'progressive' ] },
+	{ action: 'save', label: 'Insert', flags: [ /*'primary',*/ 'progressive' ] },
 	{
 		'label': OO.ui.deferMsg( 'visualeditor-dialog-action-cancel' ),
 		'flags': 'safe',
 		'modes': [ 'edit', 'insert', 'select' ]
 	}
 ];
-     var myDropdown = new OO.ui.DropdownWidget( {
-         menu: {
-             items: [
-                 new OO.ui.MenuSectionOptionWidget( {
-                     label: 'Dogs'
-                 } ),
-                 new OO.ui.MenuOptionWidget( {
-                     data: 'corgi',
-                     label: 'Welsh Corgi'
-                 } ),
-                 new OO.ui.MenuOptionWidget( {
-                     data: 'poodle',
-                     label: 'Standard Poodle'
-                 } ),
-                 new OO.ui.MenuSectionOptionWidget( {
-                     label: 'Cats'
-                 } ),
-                 new OO.ui.MenuOptionWidget( {
-                     data: 'lion',
-                     label: 'Lion'
-                 } )
-             ]
-         }
-     } );
 	this.inputsFieldset.$element.append(
-		this.toField.$element,
-		this.subjectField.$element,
-		matchCaseField.$element,
-		 radioSelectInput.$element,
-		 myDropdown.$element
+		
+		this.subjectField.$element
 	);
 	this.panel.$element.append(	this.inputsFieldset.$element );
 	this.$body.append( this.panel.$element );
