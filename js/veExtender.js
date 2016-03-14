044a10ccdc23527ecd32e1e1133339b7c60eaada
Anton <anton.bil@hz.nl>
2016-03-14 16:18:05 +0100

    redefine what tools will be initially available in the insert-menu

--- js/veExtender.js
+++ js/veExtender.js
@@ -8,7 +8,8 @@ function loadEMMExtender(){
 
 mw.hook( 've.activationComplete' ).add( function() {
 	// Register plugins to VE. will be loaded once the user opens the VE
-
+  //redefine what tools will be initially available in the insert-menu
+ve.ui.toolFactory.bindings.register[6].context.forceExpand= [ 'media', 'insertTable' ,'linkpage' ,'linkwebsite' ,'linkreference'];
     if (typeof ve === 'undefined') {
       setTimeout(function() { mw.loader.using( 'ext.visualEditor.viewPageTarget.init',loadEMMExtender); }, 1000);
       console.log('ve undefined'); 
