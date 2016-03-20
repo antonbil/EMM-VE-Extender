eb14f18d5898d5717d5d69ad732d33d50e4ce763
Anton <anton.bil@hz.nl>
2016-03-20 21:49:35 +0100

    added comments

--- js/veExtender.js
+++ js/veExtender.js
@@ -1,3 +1,4 @@
+  //redefine what tools will be initially available in the insert-menu
 function redefineMenu(){
   var force=ve.ui.toolFactory.bindings.register[6].context.forceExpand;
   //remove transclusion from neu if available
@@ -22,13 +23,6 @@ function loadEMMExtender(){
 }
 
 mw.hook( 've.activationComplete' ).add( function() {
-	// Register plugins to VE. will be loaded once the user opens the VE
-  //redefine what tools will be initially available in the insert-menu
-  //ve.ui.toolFactory.bindings.register[6].context.forceExpand= [ 'media', 'insertTable' ,'linkpage' ,'linkwebsite' ,'linkreference'];
+  // Register plugins to VE. will be loaded once the user opens the VE
   loadEMMExtender();
-  /*  if (typeof ve === 'undefined') {
-      setTimeout(function() { mw.loader.using( 'ext.visualEditor.viewPageTarget.init',loadEMMExtender); }, 1000);
-      console.log('ve undefined'); 
-      return;
-    }else loadEMMExtender();*/
 } );
